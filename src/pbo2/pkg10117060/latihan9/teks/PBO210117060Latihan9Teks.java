a54e4f4c5fd173f2c71096af2d428e7ce70b2870
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:29:41 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan9/teks/PBO210117060Latihan9Teks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi menampilkan data, yang nilainya
+ * dimasukkan kedalam variabel c. dan kemudian ada source code untuk tab, 
+ * newline sehingga program jadi lebih rapih.
+ * 
+ */
+public class PBO210117060Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char : " + tab + "berisi " + c);
+    }
+    
+}
